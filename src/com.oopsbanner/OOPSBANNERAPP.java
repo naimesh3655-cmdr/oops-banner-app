f12dac010e99c808fbd08c130c9149b640803747
naimesh3655-cmdr <naimesh3655@gmail.com>
2026-03-02 20:40:05 +0530

    Feature (UC6): Modular banner using helper methods

--- src/com.oopsbanner/OOPSBANNERAPP.java
+++ src/com.oopsbanner/OOPSBANNERAPP.java
@@ -1,33 +1,62 @@
-/**
- * OOPSBannerApp UC4 - Render OOPS as Banner using String Array and Loop
- *
- * This use case improves upon UC3 by using a String array to store banner lines
- * and iterating through them with a for-each loop, eliminating hardcoded print
- * statements and improving modularity and reusability.
- *
- * @author Naimesh Padhi
- * @version 4
- */
+
 
 public class OOPSBANNERAPP {
 
-    // Main method to run the banner display
+    // Helper Method for Letter O
+    static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "**   **",
+                "**   **",
+                "**   **",
+                "**   **",
+                "**   **",
+                " ***** "
+        };
+    }
+
+    // Helper Method for Letter P
+    static String[] getP() {
+        return new String[]{
+                "****** ",
+                "**   **",
+                "**   **",
+                "****** ",
+                "**     ",
+                "**     ",
+                "**     "
+        };
+    }
+
+    // Helper Method for Letter S
+    static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "**     ",
+                "**     ",
+                " ***** ",
+                "     **",
+                "     **",
+                " ***** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        // Define a String array with size equal to number of banner lines
-        String[] lines = new String[7];
+        String[] O1 = getO();
+        String[] O2 = getO();
+        String[] P = getP();
+        String[] S = getS();
 
-        // Populate banner lines using String.join()
-        lines[0] = String.join(" ", " ***** ", " ***** ", " ***** ", " ***** ");
-        lines[1] = String.join(" ", " *   * ", " *   * ", " *   * ", " *     ");
-        lines[2] = String.join(" ", " *   * ", " *   * ", " *   * ", " *     ");
-        lines[3] = String.join(" ", " ***** ", " ***** ", " ***** ", " ***** ");
-        lines[4] = String.join(" ", " *   * ", " *   * ", " *   * ", "     * ");
-        lines[5] = String.join(" ", " *   * ", " *   * ", " *   * ", "     * ");
-        lines[6] = String.join(" ", " ***** ", " ***** ", " ***** ", " ***** ");
+        // Build banner lines dynamically
+        String[] banner = new String[7];
 
-        // Use for-each loop to print each line
-        for (String line : lines) {
+        for (int i = 0; i < 7; i++) {
+            banner[i] = O1[i] + " " + O2[i] + " " + P[i] + " " + S[i];
+        }
+
+        // Display banner
+        for (String line : banner) {
             System.out.println(line);
         }
     }
